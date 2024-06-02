cf4073c0aa7742874ff85b2ca21f65b7c461dc62
KovidaOpatha <kovidaopathaz@gmail.com>
2024-06-02 21:52:19 +0530

    initialcommit

--- sky_solar_frontend/src/main.jsx
+++ sky_solar_frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import Loginpage from './pages/loginpage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Loginpage />
-Loginpage  </React.StrictMode>,
+    <Loginpage/>
+  </React.StrictMode>,
 )
-
