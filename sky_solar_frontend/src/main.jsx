dbb7d47b73121d7bea1c4b4d40edfe7608e55684
KovidaOpatha <kovidaopathaz@gmail.com>
2024-06-03 13:01:14 +0530

    registerpagecommit

--- sky_solar_frontend/src/main.jsx
+++ sky_solar_frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Loginpage from './pages/loginpage'
+import Registerpage from './pages/registerpage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Loginpage/>
+    <Registerpage/>
   </React.StrictMode>,
 )
